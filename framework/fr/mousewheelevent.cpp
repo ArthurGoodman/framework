204941665247e1b63358881f6d7b9489c4c2e444
ArthurGoodman <arthur.goodman3@gmail.com>
2016-07-05 10:58:13 +0300

    i said test out!

--- framework/fr/mousewheelevent.cpp
+++
@@ -1,9 +0,0 @@
-#include "mousewheelevent.h"
-
-int fr::MouseWheelEvent::delta() const {
-    return _delta;
-}
-
-fr::MouseWheelEvent::MouseWheelEvent(fr::MouseEvent::MouseEventType type, fr::MouseEvent::MouseButton button, int x, int y, uint modifiers, int delta)
-    : MouseEvent(type, button, x, y, modifiers), _delta(delta) {
-}
--- framework/fr/timerevent.cpp
+++
@@ -1,9 +0,0 @@
-#include "timerevent.h"
-
-int fr::TimerEvent::id() const {
-    return _id;
-}
-
-fr::TimerEvent::TimerEvent(int id)
-    : _id(id) {
-}
